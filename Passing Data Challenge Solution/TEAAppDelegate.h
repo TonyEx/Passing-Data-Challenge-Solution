836f4c8b5405db47f14f0ac5ee3f796619645c70
TonyEx <tonyangelox@veronica.home>
2014-07-12 15:54:49 -0400

    Initial Commit

---
+++ Passing Data Challenge Solution/TEAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TEAAppDelegate.h
+//  Passing Data Challenge Solution
+//
+//  Created by Tony Angelo on 7/12/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
